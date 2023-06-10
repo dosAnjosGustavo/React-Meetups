b3e6a6b115745bf3d1555f76e6b9bdeaa38d9169
dosanjosgustavo <lalodosanjos@gmail.com>
2023-06-10 03:52:39 -0300

    changed type

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import { AppProps } from 'next/dist/shared/lib/router/router';
 import Layout from '../components/layout/Layout';
 import '../styles/globals.css';
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({ Component, pageProps }: any) {
   return (
     <Layout>
       <Component {...pageProps} />
